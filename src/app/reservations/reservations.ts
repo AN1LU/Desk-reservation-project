5a522a79d080623a5522ba680dc91ca73c45716f
AN1LU <analuciapelayom@gmail.com>
2025-11-09 22:33:42 -0600

    add to repository feature from deliverable 2

--- src/app/reservations/reservations.ts
+++ src/app/reservations/reservations.ts
@@ -1,9 +1,80 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { QRCodeComponent } from 'angularx-qrcode';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-reservations',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './reservations.html',
-  styleUrls: ['./reservations.css']
+  styleUrl: './reservations.css'
 })
-export class ReservationsComponent {}
+
+export class ReservationsComponent {
+  escritorios = Array.from({ length: 20 }, (_, i) => i + 1); // 20 escritorios
+  mostrarQR = false;
+  qrData = '';
+
+  mostrarCalendario = false;
+  mesActual = new Date();
+  diasDelMes: Date[] = [];
+  diasSemana = ['Dom', 'Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb'];
+
+  ngOnInit() {
+    this.generarCalendario(this.mesActual);
+  }
+
+  toggleCalendario() {
+    this.mostrarCalendario = !this.mostrarCalendario;
+  }
+  generarCalendario(fecha: Date) {
+    const inicio = new Date(fecha.getFullYear(), fecha.getMonth(), 1);
+    const fin = new Date(fecha.getFullYear(), fecha.getMonth() + 1, 0);
+    const dias = [];
+
+    for (let d = inicio.getDate(); d <= fin.getDate(); d++) {
+      dias.push(new Date(fecha.getFullYear(), fecha.getMonth(), d));
+    }
+
+    this.diasDelMes = dias;
+  }
+
+  mesAnterior() {
+    this.mesActual = new Date(this.mesActual.getFullYear(), this.mesActual.getMonth() - 1, 1);
+    this.generarCalendario(this.mesActual);
+  }
+
+  mesSiguiente() {
+    this.mesActual = new Date(this.mesActual.getFullYear(), this.mesActual.getMonth() + 1, 1);
+    this.generarCalendario(this.mesActual);
+  }
+
+  esHoy(dia: Date): boolean {
+    const hoy = new Date();
+    return (
+      dia.getDate() === hoy.getDate() &&
+      dia.getMonth() === hoy.getMonth() &&
+      dia.getFullYear() === hoy.getFullYear()
+    );
+  }
+
+  seleccionarFecha(dia: Date) {
+    alert(`Has seleccionado el día ${dia.toLocaleDateString()}`);
+  }
+  
+  reservar(item: string) {
+    this.qrData = `${item} reservado exitosamente`;
+    this.mostrarQR = true;
+  }
+
+  reservarSala(nombre: string) {
+    this.qrData = `${nombre} reservada exitosamente`;
+    this.mostrarQR = true;
+  }
+
+  cerrarQR() {
+    this.mostrarQR = false;
+  }
+}
